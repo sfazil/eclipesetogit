610d30b1d93010d28fa0e8a087e942dd54a0ce8e
fazil <sfazil@users.noreply.github.com>
2018-12-11 15:41:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("editing from ecli to git modification");
+      System.out.println("editing from ecli to git modifi");
    }
 }
